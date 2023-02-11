13c6a9596722cd6187747359611d1f49c9330383
Flavio Almeida <falmeida@c2r8s1.42lisboa.com>
2023-02-11 15:22:12 +0000

    Completed

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -8,7 +8,6 @@ Base    *generate(void) {
 
     int random = rand() % 4;
 
-    std::cout << random << std::endl;
     switch (random)
     {
     case 1:
@@ -28,13 +27,39 @@ Base    *generate(void) {
 
 void    identify(Base *p) {
 
-    if (dynamic_cast<A*>(p) != NULL)
+    if (dynamic_cast<A*>(p) != nullptr)
         std::cout << "Base class type A" << std::endl;
-    else if (dynamic_cast<B*>(p) != NULL)
-        std::cout << "Base class type A" << std::endl;
-    else if (dynamic_cast<C*>(p) != NULL)
-        std::cout << "Base class type A" << std::endl;
+    else if (dynamic_cast<B*>(p) != nullptr)
+        std::cout << "Base class type B" << std::endl;
+    else if (dynamic_cast<C*>(p) != nullptr)
+        std::cout << "Base class type C" << std::endl;
+    else
+        std::cout << "Not from Base class" << std::endl;
+}
 
+void    identify(Base& p) {
+
+    try {
+        A a;
+        a = dynamic_cast<A&>(p);
+        std::cout << "Base class type A" << std::endl;
+    } catch (const std::exception& e)
+    {
+    }
+    try {
+        B b;
+        b = dynamic_cast<B&>(p);
+        std::cout << "Base class type B" << std::endl;
+    } catch (const std::exception& e)
+    {
+    }
+    try {
+        C c;
+        c = dynamic_cast<C&>(p);
+        std::cout << "Base class type C" << std::endl;
+    } catch (const std::exception& e)
+    {
+    }
 }
 
 int main () {
@@ -42,8 +67,11 @@ int main () {
     Base *b1 = generate();
     Base *b2 = generate();
     Base *b3 = generate();
-
+  
     identify(b1);
     identify(b2);
     identify(b3);
+    identify(*b1);
+    identify(*b2);
+    identify(*b3);
 }
